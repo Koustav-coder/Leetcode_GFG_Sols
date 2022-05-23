b857c5aaa9731a164b7b848784d7dd9097f5520c
Koustav Ghosh <65316907+Koustav-coder@users.noreply.github.com>
2022-05-23 09:37:50 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.java
@@ -0,0 +1,96 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+   ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+   {
+       ArrayList<Integer> list = new ArrayList<>();
+       
+       int i,j,k;
+       i=0;
+       j=0;
+       k=0;
+       
+       while(i<n1 && j<n2 && k<n3){
+           if(B[j]>A[i] || C[k]>A[i])
+              i++;
+           else if(B[j] == A[i] && C[k]<A[i]){
+               k++;
+           }   
+           else if(C[k] == A[i] && B[j] < A[i])
+             j++;
+           else if(B[j] == A[i] && C[k] == A[i]){
+               if(!list.contains(A[i]))
+               list.add(A[i]);
+               i++;
+               j++;
+               k++;
+           }
+           else
+           {
+               j++;
+               k++;
+           }
+           
+       }
+       
+       return list;
+       
+       
+   }
+}
