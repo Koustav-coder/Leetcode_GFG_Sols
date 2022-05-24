c03ce49ca1fcdfd9ec6acf5e3fcef807741c0191
Koustav Ghosh <65316907+Koustav-coder@users.noreply.github.com>
2022-05-24 17:48:57 +0530

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        int key = 10_000_001;
+        
+        for (int i = 0 ; i < (n >> 1) ; i++) {
+            arr[i] = arr[i] + arr[n-i-1] * key;
+        }
+        
+        if ((n & 1) == 1) {
+            arr[n - 1] = arr[n >> 1];
+            n--;
+        }
+        
+        for (int i = (n >> 1) - 1 ; i >= 0 ; i--) {
+            arr[--n] = arr[i] % key;
+            arr[--n] = arr[i] / key;
+        
+    }
+    
+}
+}
+
+
