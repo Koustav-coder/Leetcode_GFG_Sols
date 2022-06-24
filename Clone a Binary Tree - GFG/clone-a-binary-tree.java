96d283bccea03c0011e355f2b1ab767428fa5fda
Koustav Ghosh <65316907+Koustav-coder@users.noreply.github.com>
2022-06-24 17:21:39 +0530

    Added solution - LeetHub

---
+++ Clone a Binary Tree - GFG/clone-a-binary-tree.java
@@ -0,0 +1,178 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+class Tree{
+	int data;
+	Tree left,right,random;
+	Tree(int d){
+		data=d;
+		left=null;
+		right=null;
+		random=null;
+	}
+}
+class CloneABT{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int q=sc.nextInt();
+		int n,n1,n2;
+		char lr;
+		Tree root;
+		while(q-->0){
+			n=sc.nextInt();
+			root=null;
+			Solution g=new Solution();
+			while(n-->0){
+				n1=sc.nextInt();
+				n2=sc.nextInt();
+				lr=sc.next().charAt(0);
+				if(root==null){
+					root=new Tree(n1);
+					switch(lr){
+						case 'L':root.left=new Tree(n2);
+						break;
+						case 'R':root.right=new Tree(n2);
+						break;
+					}
+				}
+				else{
+					if(lr=='X')
+						clone(root,root,n1,n2);
+					else
+					insert(root,n1,n2,lr);
+				}
+			}
+		try{Tree z=g.cloneTree(root);
+	if(z==root)
+        System.out.println("0");
+     else
+     {
+         if(printInorder(root,z))
+            System.out.println("1");
+            
+        else
+            System.out.println("0");
+            
+     }
+		}catch(ClassCastException ex){}
+		}
+	}
+public static boolean printInorder(Tree a,Tree b){
+    if((a==null) && (b==null))
+        return true;
+        
+    if(a!=null && b!=null)
+    {
+       boolean check=((a.data==b.data) && printInorder(a.left,b.left) && printInorder(a.right,b.right));
+        
+        if(a.random!=null && b.random!=null)
+            return (check && (a.random.data==b.random.data));
+            
+        if(a.random==b.random)
+            return check;
+            
+        return false;
+        
+    }
+    
+   /* if(a.random.data == b.random.data) 
+		if(printInorder(a.left,b.left)==1) 
+			if(printInorder(a.right,b.right)==1)
+				return 1;*/
+    return false;
+
+}
+public static void clone(Tree root,Tree proot,int n1,int n2){
+	try{if(root==null && proot==null)
+		return;
+	if(n1==root.data){
+		if(proot.data==n2)
+			root.random=proot;
+		else{
+			clone(root,proot.left,n1,n2);
+			clone(root,proot.right,n1,n2);
+		}
+	}
+	else{
+		clone(root.left,proot,n1,n2);
+		clone(root.right,proot,n1,n2);
+	}}catch(NullPointerException ex){}
+}
+public static void insert(Tree root,int n1,int n2,char lr){
+	if(root==null)
+		return;
+	if(n1==root.data){
+		switch(lr){
+						case 'L':root.left=new Tree(n2);
+						break;
+						case 'R':root.right=new Tree(n2);
+						break;
+					}
+	}
+	else{
+		insert(root.left,n1,n2,lr);
+		insert(root.right,n1,n2,lr);
+	}
+}
+}// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+class Tree{
+	int data;
+	Tree left,right,random;
+	Tree(int d){
+		data=d;
+		left=null;
+		right=null;
+		random=null;
+	}
+}*/
+class Solution {
+   
+   public Tree cloneTree(Tree tree){
+       addDuplicate(tree) ;
+       addRandom(tree) ;
+       Tree dup = removeDup(tree) ;
+       return dup ;
+   }
+   public void addDuplicate(Tree node){
+       if(node == null){
+           return ;
+       }
+       
+       addDuplicate(node.left) ;
+       addDuplicate(node.right) ;
+       
+       Tree temp = new Tree( node.data ) ;
+       temp.left = node.left ;
+       node.left = temp ;
+   }
+   
+   public void addRandom(Tree node){
+       if(node == null){
+           return ;
+       }
+       addRandom(node.left.left) ;
+       addRandom(node.right) ;
+       if(node.random != null){
+           node.left.random = node.random.left ;
+       }
+   }
+   
+   public Tree removeDup(Tree node){
+       if(node == null){
+           return null ;
+       }
+       Tree left = removeDup(node.left.left) ;
+       Tree right = removeDup(node.right) ;
+       
+       Tree dup = node.left ;
+       node.left = node.left.left ;
+       dup.left = left ;
+       dup.right = right ;
+       
+       return dup ;
+   }
+}
