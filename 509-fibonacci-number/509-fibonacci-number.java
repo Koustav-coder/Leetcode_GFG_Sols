ca634686fdb780164ff284a2ecc6e983677f5ee3
Koustav Ghosh <65316907+Koustav-coder@users.noreply.github.com>
2022-07-06 16:02:06 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (49.33%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+      if(n <= 1) return n;
+      
+     int a = 0, b = 1;
+      
+      while(n -- > 1) {
+        int sum = a + b;
+        a = b;
+        b = sum;
+      }
+      return b;
+    }
+}
